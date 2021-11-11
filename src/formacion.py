2fae36bf7abefaae46b9b0bb11efa02478cac68c
Eduardo Segura Richart <edusr@correo.ugr.es>
2021-11-11 18:02:15 +0100

    Creada clase enum Formacion con los valores correspondientes (#12)
    Se ha creado el enumerado para la representación de las formaciones en las que puede jugar un equipo. Estas pueden ser 3-4-3, 3-5-2, 4-2-3-1, 4-3-3, 4-4-2, 5-4-1 y 5-3-2. >

---
+++ src/formacion.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Formacion(Enum):
+    """
+    Clase para representar la formacion
+    """
+
+    TRES_CUATRO_TRES = '3-4-3'
+    TRES_CINCO_DOS = '3-5-2'
+    CUATRO_DOS_TRES_UNO = '4-2-3-1'
+    CUATRO_TRES_TRES = '4-3-3'
+    CUATRO_CUATRO_DOS = '4-4-2'
+    CINCO_CUATRO_UNO = '5-4-1'
+    CINCO_TRES_DOS = '5-3-2'
